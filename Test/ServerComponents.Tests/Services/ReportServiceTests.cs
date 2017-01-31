01ff4991ca015269e212bce990ecfd5478ac924b
Isaiah Lee <leesisaiah@gmail.com>
2017-01-30 21:42:31 -0800

    add solutions for practice report service tests

--- Test/ServerComponents.Tests/Services/ReportServiceTests.cs
+++ Test/ServerComponents.Tests/Services/ReportServiceTests.cs
@@ -1,9 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Moq;
 using ServerAbstractions;
 using ServerAbstractions.Services;
 using ServerComponents.Services;
 using TestsCore;
+using Xunit;
+using ServerAbstractions.SqlEntities;
+using CoreAbstractions.Entities;
 
 namespace ServerComponents.Tests.Services
 {
@@ -17,39 +21,185 @@ namespace ServerComponents.Tests.Services
 			return testingObject;
 		}
 
+        [Fact]
 	    public async Task GetAsync_InvalidArgument_ThrowsArgumentException()
 	    {
-		    
-	    }
+            TestingObject<ReportService> testingObject = this.GetTestingObject();
 
-	    public async Task GetAsync_ReportNotFound_ThrowsException()
-	    {
-		    
-	    }
+            ReportService reportService = testingObject.GetResolvedTestingObject();
 
-	    public async Task GetAsync_ExpiredReport_ThrowsException()
-	    {
-		    
-	    }
-
-        public async Task GetAsync_OwnerNotFound_ThrowsException()
-        {
-
+            await Assert.ThrowsAsync<ArgumentException>(async () =>
+                await reportService.GetAsync(Guid.Empty));
         }
 
-	    public async Task GetAsync_OwnerDisabled_ThrowsException()
+        [Fact]
+        public async Task GetAsync_ReportNotFound_ThrowsException()
 	    {
-		    
-	    }
+            TestingObject<ReportService> testingObject = this.GetTestingObject();
 
-	    public async Task GetAsync_Success_Updated_ReturnsCorrectResult()
-	    {
-		    
-	    }
+            Guid reportIdArg = Guid.NewGuid();
 
-	    public async Task GetAsync_Success_NotUpdated_ReturnsCorrectResult()
+            var mockDbContext = testingObject.GetDependency<Mock<IDatabaseContext>>();
+            mockDbContext
+                .Setup(m => m.FindSingleAsync<ReportMetadataSql>(It.Is<Guid>(id => id == reportIdArg)))
+                .ReturnsAsync(null);
+
+            ReportService reportService = testingObject.GetResolvedTestingObject();
+            await Assert.ThrowsAsync<Exception>(async ()
+               => await reportService.GetAsync(reportIdArg));
+        }
+
+        [Fact]
+        public async Task GetAsync_ExpiredReport_ThrowsException()
 	    {
-		    
-	    }
+            TestingObject<ReportService> testingObject = this.GetTestingObject();
+
+            Guid reportIdArg = Guid.NewGuid();
+
+            var mockDbContext = testingObject.GetDependency<Mock<IDatabaseContext>>();
+
+            var report = new ReportMetadataSql
+            {
+                Created = DateTime.UtcNow.AddYears(-4)
+            };
+            mockDbContext
+                .Setup(m => m.FindSingleAsync<ReportMetadataSql>(It.Is<Guid>(id => id == reportIdArg)))
+                .ReturnsAsync(report);
+
+            ReportService reportService = testingObject.GetResolvedTestingObject();
+            await Assert.ThrowsAsync<Exception>(async ()
+               => await reportService.GetAsync(reportIdArg));
+        }
+
+        [Fact]
+        public async Task GetAsync_OwnerNotFound_ThrowsException()
+        {
+            TestingObject<ReportService> testingObject = this.GetTestingObject();
+
+            Guid reportIdArg = Guid.NewGuid();
+
+            var mockDbContext = testingObject.GetDependency<Mock<IDatabaseContext>>();
+
+            var report = new ReportMetadataSql
+            {
+                Created = DateTime.UtcNow.AddYears(-2),
+                OwnerId = Guid.NewGuid()
+            };
+            mockDbContext
+                .Setup(m => m.FindSingleAsync<ReportMetadataSql>(It.Is<Guid>(id => id == reportIdArg)))
+                .ReturnsAsync(report);
+
+            mockDbContext
+                .Setup(m => m.FindSingleAsync<UserSql>(It.Is<Guid>(id => id == report.OwnerId)))
+                .ReturnsAsync(null);
+
+            ReportService reportService = testingObject.GetResolvedTestingObject();
+            await Assert.ThrowsAsync<Exception>(async ()
+               => await reportService.GetAsync(reportIdArg));
+        }
+
+        [Fact]
+        public async Task GetAsync_OwnerDisabled_ThrowsException()
+	    {
+            TestingObject<ReportService> testingObject = this.GetTestingObject();
+
+            Guid reportIdArg = Guid.NewGuid();
+
+            var mockDbContext = testingObject.GetDependency<Mock<IDatabaseContext>>();
+
+            var report = new ReportMetadataSql
+            {
+                Created = DateTime.UtcNow.AddYears(-2),
+                OwnerId = Guid.NewGuid()
+            };
+            mockDbContext
+                .Setup(m => m.FindSingleAsync<ReportMetadataSql>(It.Is<Guid>(id => id == reportIdArg)))
+                .ReturnsAsync(report);
+
+            var owner = new UserSql
+            {
+                Enabled = false
+            };
+            mockDbContext
+                .Setup(m => m.FindSingleAsync<UserSql>(It.Is<Guid>(id => id == report.OwnerId)))
+                .ReturnsAsync(owner);
+
+            ReportService reportService = testingObject.GetResolvedTestingObject();
+            await Assert.ThrowsAsync<Exception>(async ()
+               => await reportService.GetAsync(reportIdArg));
+        }
+
+        [Fact]
+        public async Task GetAsync_Success_Updated_ReturnsCorrectResult()
+	    {
+            TestingObject<ReportService> testingObject = this.GetTestingObject();
+
+            Guid reportIdArg = Guid.NewGuid();
+
+            var mockDbContext = testingObject.GetDependency<Mock<IDatabaseContext>>();
+
+            var report = new ReportMetadataSql
+            {
+                Created = DateTime.UtcNow.AddYears(-2),
+                OwnerId = Guid.NewGuid(),
+                LastUpdated = DateTime.UtcNow,
+                Title = "Report Title"
+            };
+            mockDbContext
+                .Setup(m => m.FindSingleAsync<ReportMetadataSql>(It.Is<Guid>(id => id == reportIdArg)))
+                .ReturnsAsync(report);
+
+            var owner = new UserSql
+            {
+                Enabled = true,
+                FirstName = "Mary",
+                LastName = "Jane"
+            };
+            mockDbContext
+                .Setup(m => m.FindSingleAsync<UserSql>(It.Is<Guid>(id => id == report.OwnerId)))
+                .ReturnsAsync(owner);
+
+            ReportService reportService = testingObject.GetResolvedTestingObject();
+
+            ReportMetadata result = await reportService.GetAsync(reportIdArg);
+
+            Assert.EndsWith("(Revision)", result.Title);
+        }
+
+        [Fact]
+        public async Task GetAsync_Success_NotUpdated_ReturnsCorrectResult()
+	    {
+            TestingObject<ReportService> testingObject = this.GetTestingObject();
+
+            Guid reportIdArg = Guid.NewGuid();
+
+            var mockDbContext = testingObject.GetDependency<Mock<IDatabaseContext>>();
+
+            var report = new ReportMetadataSql
+            {
+                Created = DateTime.UtcNow.AddYears(-2),
+                OwnerId = Guid.NewGuid(),
+                Title = "Report Title"
+            };
+            mockDbContext
+                .Setup(m => m.FindSingleAsync<ReportMetadataSql>(It.Is<Guid>(id => id == reportIdArg)))
+                .ReturnsAsync(report);
+
+            var owner = new UserSql
+            {
+                Enabled = true,
+                FirstName = "Mary",
+                LastName = "Jane"
+            };
+            mockDbContext
+                .Setup(m => m.FindSingleAsync<UserSql>(It.Is<Guid>(id => id == report.OwnerId)))
+                .ReturnsAsync(owner);
+
+            ReportService reportService = testingObject.GetResolvedTestingObject();
+
+            ReportMetadata result = await reportService.GetAsync(reportIdArg);
+
+            Assert.EndsWith("(Original)", result.Title);
+        }
 	}
 }
